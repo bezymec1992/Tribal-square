0430f9577b614d4865028dcddac41509c59ee207
sergashs <sergas123456@gmail.com>
2022-06-16 14:00:28 +0300

    clear all

--- .nuxt/fontLoader.js
+++
@@ -1,8 +0,0 @@
-export default function () {
-  window.onNuxtReady(() => {
-      const link = document.createElement('link')
-      link.rel = 'stylesheet'
-      link.href = '/fonts/stylesheet.css'
-      document.head.appendChild(link)
-  })
-}
